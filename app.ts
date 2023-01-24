52ccc12f6d3ff08a5563452232d9b4998e09450d
Water Genie <thirdwatergenie@gmail.com>
2023-01-24 23:29:26 +0700

    Add basic setup for eslint, prettier, and husky (test commit)

--- app.ts
+++ app.ts
@@ -1,80 +1,73 @@
-// constants
-// see https://www.typescriptlang.org/docs/handbook/enums.html#objects-vs-enums
-// const OEventType = {
-//     // Revert everything back to strings if these are dynamic.
-//     Sale: "SALE",
-//     Refill: "REFILL",
-//     LowStockWarning: "LOW_STOCK_WARNING",
-//     StockLevelOk: "STOCK_LEVEL_OK",
-// } as const;
-// type EventType = typeof OEventType[keyof typeof OEventType];
+// constants/enums
 enum EventType {
-    Sale,
-    Refill,
-    LowStockWarning,
-    StockLevelOk,
+  Sale,
+  Refill,
+  LowStockWarning,
+  StockLevelOk,
 }
-
 
 // interfaces
 interface IEvent {
-  type(): EventType;
-  machineId(): string;
+  type: () => EventType;
+  machineId: () => string;
 }
 
 interface ISubscriber {
-  handle(event: IEvent): void;
+  handle: (event: IEvent) => void;
 }
 
 interface IPublishSubscribeService {
-  publish(event: IEvent): void;
-  subscribe(type: EventType, handler: ISubscriber): void;
-  unsubscribe(type: EventType, handler: ISubscriber): void;
+  publish: (event: IEvent) => void;
+  subscribe: (type: EventType, handler: ISubscriber) => void;
+  unsubscribe: (type: EventType, handler: ISubscriber) => void;
 }
-
 
 // implementations
 class PublishSubscribeService implements IPublishSubscribeService {
-    #subscriptions: Partial<Record<EventType, ISubscriber[]>> = {};
+  private readonly subscriptions: Partial<Record<EventType, ISubscriber[]>> =
+    {};
 
-    publish(event: IEvent) {
-        let eventType = event.type();
-        if (eventType in this.#subscriptions) {
-            this.#subscriptions[eventType]?.forEach((subscriber) => {
-                subscriber.handle(event);
-            });
-        }
+  publish(event: IEvent): void {
+    const eventType = event.type();
+    if (eventType in this.subscriptions) {
+      this.subscriptions[eventType]?.forEach((subscriber) => {
+        subscriber.handle(event);
+      });
     }
+  }
 
-    subscribe(eventType: EventType, handler: ISubscriber) {
-        let subscribers = this.#subscriptions[eventType];
-        if (subscribers === undefined) {
-            subscribers = [handler];
-        } else if (subscribers.indexOf(handler) !== -1) {
-            subscribers.push(handler);
-        }
+  subscribe(eventType: EventType, handler: ISubscriber): void {
+    let subscribers = this.subscriptions[eventType];
+    if (subscribers === undefined) {
+      subscribers = [handler];
+    } else if (subscribers.indexOf(handler) !== -1) {
+      subscribers.push(handler);
     }
+  }
 
-    unsubscribe(eventType: EventType, handler: ISubscriber) {
-        let subscribers = this.#subscriptions[eventType];
-        if (subscribers !== undefined) {
-            let handlerIndex = subscribers.indexOf(handler);
-            if (handlerIndex !== -1) {
-                subscribers.splice(handlerIndex, 1);
-            }
-        }
+  unsubscribe(eventType: EventType, handler: ISubscriber): void {
+    const subscribers = this.subscriptions[eventType];
+    if (subscribers !== undefined) {
+      const handlerIndex = subscribers.indexOf(handler);
+      if (handlerIndex !== -1) {
+        subscribers.splice(handlerIndex, 1);
+      }
     }
+  }
 }
 
 class MachineSaleEvent implements IEvent {
-  constructor(private readonly _sold: number, private readonly _machineId: string) {}
+  constructor(
+    private readonly _sold: number,
+    private readonly _machineId: string
+  ) {}
 
   machineId(): string {
     return this._machineId;
   }
 
   getSoldQuantity(): number {
-    return this._sold
+    return this._sold;
   }
 
   type(): EventType {
@@ -83,7 +76,10 @@ class MachineSaleEvent implements IEvent {
 }
 
 class MachineRefillEvent implements IEvent {
-  constructor(private readonly _refill: number, private readonly _machineId: string) {}
+  constructor(
+    private readonly _refill: number,
+    private readonly _machineId: string
+  ) {}
 
   machineId(): string {
     throw new Error("Method not implemented.");
@@ -100,12 +96,14 @@ class MachineRefillEvent implements IEvent {
 class MachineSaleSubscriber implements ISubscriber {
   public machines: Machine[];
 
-  constructor (machines: Machine[]) {
-    this.machines = machines; 
+  constructor(machines: Machine[]) {
+    this.machines = machines;
   }
 
-  handle(event: MachineSaleEvent): void {
-    this.machines[2].stockLevel -= event.getSoldQuantity();
+  handle(event: IEvent): void {
+    if (event instanceof MachineSaleEvent) {
+      this.machines[2].stockLevel -= event.getSoldQuantity();
+    }
   }
 }
 
@@ -118,54 +116,55 @@ class MachineRefillSubscriber implements ISubscriber {
   }
 }
 
-
 // objects
 class Machine {
   public stockLevel = 10;
   public id: string;
 
-  constructor (id: string) {
+  constructor(id: string) {
     this.id = id;
   }
 }
-
 
 // helpers
 const randomMachine = (): string => {
   const random = Math.random() * 3;
   if (random < 1) {
-    return '001';
+    return "001";
   } else if (random < 2) {
-    return '002';
+    return "002";
   }
-  return '003';
-
-}
+  return "003";
+};
 
 const eventGenerator = (): IEvent => {
   const random = Math.random();
   if (random < 0.5) {
     const saleQty = Math.random() < 0.5 ? 1 : 2; // 1 or 2
     return new MachineSaleEvent(saleQty, randomMachine());
-  } 
+  }
   const refillQty = Math.random() < 0.5 ? 3 : 5; // 3 or 5
   return new MachineRefillEvent(refillQty, randomMachine());
-}
-
+};
 
 // program
 (async () => {
   // create 3 machines with a quantity of 10 stock
-  const machines: Machine[] = [ new Machine('001'), new Machine('002'), new Machine('003') ];
+  const machines: Machine[] = [
+    new Machine("001"),
+    new Machine("002"),
+    new Machine("003"),
+  ];
 
   // create a machine sale event subscriber. inject the machines (all subscribers should do this)
   const saleSubscriber = new MachineSaleSubscriber(machines);
 
   // create the PubSub service
-  const pubSubService: IPublishSubscribeService = null as unknown as IPublishSubscribeService; // implement and fix this
+  const pubSubService: IPublishSubscribeService =
+    null as unknown as IPublishSubscribeService; // implement and fix this
 
   // create 5 random events
-  const events = [1,2,3,4,5].map(i => eventGenerator());
+  const events = [1, 2, 3, 4, 5].map((i) => eventGenerator());
 
   // publish the events
   events.map(pubSubService.publish);
